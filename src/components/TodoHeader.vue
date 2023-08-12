e4be407ef7d0d7a80fe91eeb7efb896c6280e00f
kiraGit67 <kirsten@sais.de>
2023-08-12 20:31:41 +0200

    adds TodoHeader component

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <header class="header">
+    <h1 class="header__title">{{ headerTitle }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "TodoHeader",
+  props: {
+    headerTitle: String,
+  },
+};
+</script>
+
+<style scoped>
+.header {
+  text-align: center;
+  background-color: var(--pink);
+  padding: 3rem 1rem;
+  color: white;
+  margin-bottom: 1rem;
+}
+
+.header__title {
+  margin: 0;
+  text-shadow: 2px 2px var(--light-blue), 4px 4px var(--yellow);
+}
+</style>
