f0a78b7f25cf9841f79a8f5463d07ec78353a544
kiraGit67 <kirsten@sais.de>
2023-08-14 09:54:21 +0200

    adds DeleteButton component for deleting done todos

---
+++ src/components/DeleteButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <button class="button" id="delete-button">{{ buttonText }}</button>
+</template>
+
+<script>
+export default {
+  name: "DeleteButton",
+  props: {
+    buttonText: String,
+  },
+};
+</script>
+
+<style scoped></style>
